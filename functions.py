56916ff9f1a5c5a1496eb50b01062e6799576caf
David Šetek <david.setek40@gmail.com>
2023-01-05 21:04:03 +0100

    Initial commit 3

--- functions.py
+++ functions.py
@@ -1,13 +1,13 @@
 def get_todos(filepath="todos.txt"):
     """Read a text file and return list of to-do items """
-    with open(filepath, "r") as file_local:
+    with open(filepath, "r", encoding="utf-8") as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
 def write_todos(todos_arg, filepath="todos.txt"):
     """Write to-do items list in the text file"""
-    with open(filepath, "w") as file:
+    with open(filepath, "w", encoding="utf-8") as file:
         file.writelines(todos_arg)
 
 
